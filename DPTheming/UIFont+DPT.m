18df4fd3d92a81566277532bc915511db05f8ac7
David Pettigrew <dpettigrew@burnsidedigital.com>
2014-07-09 14:35:56 -0700

    extra logging

--- DPTheming/UIFont+DPT.m
+++ DPTheming/UIFont+DPT.m
@@ -77,6 +77,9 @@
             NSLog(@"Warning - Unsupported preferred font requested");
             break;
     }
+    if (!preferredFont) {
+        NSLog(@"Warning - font not found - %d", styleName);
+    }
     return preferredFont;
 }
 
